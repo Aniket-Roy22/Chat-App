c662d4a38f34e5484beea07d90cbc29585304285
Aniket Roy <royaniket2212@gmail.com>
2026-05-08 00:35:03 +0530

    Fix access tokens to include id with username

--- server/controllers/regenAccessToken.js
+++ server/controllers/regenAccessToken.js
@@ -2,8 +2,7 @@ import { generateAccessToken } from "../utils/generateToken.js";
 
 export function regenAccessToken(req, res)
 {
-	const user = req.user;
-	const payload = { username: user.username };
+	const payload = req.user;
 
 	const newAccessToken = generateAccessToken(payload);
 	res.status(200).json({
