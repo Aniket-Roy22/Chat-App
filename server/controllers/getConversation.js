afce7823d59d78bdc8380dfd2bd48c433f1ff6ec
Aniket Roy <royaniket2212@gmail.com>
2026-05-11 02:16:45 +0530

    Add api endpoint to get conversations between users

---
+++ server/controllers/getConversation.js
@@ -0,0 +1,63 @@
+import prisma from "../config/prisma.js";
+
+export async function getConversation(req, res)
+{
+	try
+	{
+		const currentUserId = req.user.id;
+		const {otherUsername} = req.params;
+		const otherUser = await prisma.users.findUnique({
+			where: {
+				username: otherUsername,
+			},
+			select: {
+				id: true,
+			}
+		});
+		const otherUserId = otherUser?.id;
+		
+		if (!otherUserId) {
+			return res.status(404).json({
+				message: "USER_NOT_FOUND",
+			});
+		}
+
+		const messages = await prisma.messages.findMany({
+			where: {
+				OR: [
+					{
+						sender_id: currentUserId,
+						receiver_id: otherUserId,
+					},
+					{
+						sender_id: otherUserId,
+						receiver_id: currentUserId,
+					},
+				],
+			},
+			select: {
+				sender_id: true,
+				receiver_id: true,
+				message_text: true,
+				created_at: true,
+			},
+			orderBy: {
+				created_at: "asc",
+			},
+		});
+
+		return res.status(200).json({
+			success: true,
+			data: messages,
+		});
+	}
+	catch (error)
+	{
+		console.error("getConversations error: ", error);
+		
+		return res.status(500).json({
+			success: false,
+			message: "INTERNAL_SERVER_ERROR",
+		});
+	}
+}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,24 +7,11 @@ import {localAuth} from "../middleware/localAuth.js";
 import {registerUser} from "../middleware/registerUser.js";
 import {issueTokens} from "../controllers/issueTokens.js";
 import {regenAccessToken} from "../controllers/regenAccessToken.js";
+import {getConversation} from "../controllers/getConversation.js";
 
 const router = express.Router();
-const posts = [
-	{
-		username: "Ani",
-		title: "Post 1",
-	},
-	{
-		username: "Ash",
-		title: "Post 2",
-	},
-];
 
-router.get("/posts", authenticateAccessToken, (req, res) => {
-	res.status(200).json(
-		posts.filter((post) => post.username === req.user.username),
-	);
-});
+router.get("/chat/:otherUsername", authenticateAccessToken, getConversation);
 router.post("/login", localAuth, issueTokens);
 router.post("/register", registerUser, issueTokens);
 router.post("/token", authenticateRefreshToken, regenAccessToken);
